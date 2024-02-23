3c35dc61109b8ce7769b951cffa53598b6dd516b
lu-papagni <papagniluca4@gmail.com>
2024-02-23 11:37:00 +0100

    Configurazioni varie aggiornate
    hyprland, kitty, nvim e sway*

--- hypr/script/clamshell.sh
+++ hypr/script/clamshell.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-if grep -q open /proc/acpi/button/lid/LID/state; then
+STATE="/proc/acpi/button/lid/LID0/state"
+if cat $STATE | grep -q "closed";  then
+  echo "Disabilita"
   hyprctl keyword monitor "eDP-1, disable"
 else
+  echo "Abilita"
   hyprctl keyword monitor "eDP-1,1920x1080@60,auto,1"
 fi
 
