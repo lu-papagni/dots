60a48549fc0fc153a41cb92cdb53d310df9b228c
lu-papagni <89859659+lu-papagni@users.noreply.github.com>
2024-02-23 22:11:11 +0100

    Update kickstart.sh
    Troppa roba. Comunque ora è meglio

--- kickstart.sh
+++ kickstart.sh
@@ -11,15 +11,46 @@ LI_ERROR="\e[1;31m::\e[0m"
 HI_DIM="\e[1;30m"    # Grigio
 HI_RST="\e[0m"       # Reset
 
+# Directory da cui lo script si sta eseguendo
+SCRIPT_DIR="$(dirname "$0")"
+
 # rende il testo grigio
-function DIM_TEXT () {
+function DIM_TEXT {
     echo "$HI_DIM$1$HI_RST"
+}
+
+# crea una scritta di warning
+function WARN_TEXT {
+    echo -e "\e[1;33mWARNING> $1$HI_RST"
+}
+
+# crea una scritta di suggerimento
+function TIP_TEXT {
+    echo -e "\e[1;32mTIP> $1$HI_RST"
+}
+
+# esegue o mostra in versione debug un comando
+function RUN {
+    if [ "$DEBUG" = true ]; then
+        echo -e "$(DIM_TEXT 'DEBUG>') $1"
+    else
+        $1
+    fi
+}
+
+function USAGE {
+    echo "
+    Comandi disponibili:
+    -d    ->    Modalità debug: i comandi non vengono eseguiti ma mostrati a schermo
+    -s    ->    Non controlla se il package manager specificato è installato.
+    -h    ->    Aiuto: mostra questo messaggio
+    "
+    exit 0
 }
 
 # pacchetti da installare
 RequiredPackages=(
     'neovim'    # editor di testo
-    'firefox'   # browser
     'kitty'     # terminale
     'zsh'       # shell
     'bat'       # alternativa per cat (evidenzia la sintassi del linguaggio)
@@ -34,9 +65,8 @@ RequiredPackages=(
     'pavucontrol'    # audio manager
     'light'     # gestore della retroilluminazione
     'nm-applet' # menu di rete
-    'eog'       # visualizzatore immagini
     'wlogout'   # menu logout
-    'vlc'       # visualizzatore video
+    'mpv'       # visualizzatore video
     'lsd'       # alternativa a ls
     'btop'      # monitor risorse
     'fastfetch' # fetch tool
@@ -65,11 +95,6 @@ Flatpak=(
     'spotify'
 )
 
-# collegamenti simbolici
-declare -A Symlinks=(
-    ["~/.zshrc"]="~/.config/.zshrc"
-)
-
 ################################   ESECUZIONE   #####################################
 
 echo "
@@ -83,18 +108,26 @@ echo "
 "
 
 # se sono stati passati argomenti allo script
-if [ ! -z $1 ]; then
-    case $1 in
-        "--dry-run")
-            DEBUG="echo"    # cosa fare con i comandi da runnare: di default li esegue, ma li può anche solo mostrare su stdout per debug
-            echo -e "$LI_INPUT Attenzione: lo script è in modalità test. I comandi importanti non verranno eseguiti, ma stampati."
+while getopts ":dsh" _SEP
+do
+    case $_SEP in
+        d)
+            DEBUG=true    # cosa fare con i comandi da runnare: di default li esegue, ma li può anche solo mostrare su stdout per debug
+            WARN_TEXT "lo script è in modalità test. I comandi importanti non verranno eseguiti, ma stampati."
+            ;;
+        s)
+            PKG_MAN_INSTALLED=true
+            WARN_TEXT "è stato disabilitato il controllo sull'effettiva presenza del package manager."
+            ;;
+        h)
+            USAGE
             ;;
         *)
             echo -e "$LI_ERROR '$1' non è un parametro valido."
             exit -1
             ;;
     esac
-fi
+done
 
 # input nome del package manager
 read -p "$(echo -e "$LI_INPUT Package manager utilizzato:") " manager
@@ -116,9 +149,11 @@ esac
 
 # controlla se il package manager è corretto
 which $manager > /dev/null
-if [ $? -ne 0 ]; then
-    echo -e "$LI_ERROR Il package manager '$manager' non esiste sul sistema"
-    exit -1
+if [ "$PKG_MAN_INSTALLED" = true ]; then
+    if [ $? -ne 0 ]; then
+        echo -e "$LI_ERROR Il package manager '$manager' non esiste sul sistema"
+        exit -1
+    fi
 fi
 
 # conferma per continuare
@@ -126,51 +161,66 @@ read -p "$(echo -e "$LI_INPUT Pronto per l'esecuzione. Continuare? (Y/N): ")" co
 
 # aggiorna il sistema
 echo -e "$LI_INFO Esecuzione aggiornamento completo del sistema..."
-$DEBUG sudo $manager $update_cmd
+RUN "sudo $manager $update_cmd"
+
+# Lettura dei pacchetti da installare
+TIP_TEXT "Stanno per essere installati solo i pacchetti strettamente necessari."
+TIP_TEXT "I file devono essere nella stessa directory di questo script."
+
+while read -p "$(echo -e "$LI_INPUT Vuoi indicare un nuovo file da cui leggere i pacchetti? (Y/N) ")" usePackageFile && \
+[[ $usePackageFile == [yY] || $usePackageFile == [yY][eE][sS] ]]
+do
+    read -p "$(echo -e "$LI_INPUT Nome file: ")" packageFile
+    while read -r line
+    do
+        echo -e "$LI_INFO Aggiungo: $(DIM_TEXT "$line")"
+        RequiredPackages+=("$line")
+    done < "$SCRIPT_DIR/$packageFile"
+done
 
 # installa i pacchetti di sistema
 echo -e "$LI_INFO Installazione pacchetti..."
-$DEBUG sudo $manager $install_cmd $(
+RUN "sudo $manager $install_cmd $(
     for pkg in ${RequiredPackages[*]}; do
         printf '%s ' $pkg
     done
-)
+)"
 
 echo -e "$LI_INFO Configurazione Flathub remote..."
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 echo -e "$LI_INFO Installazione flatpak..."
-$DEBUG sudo flatpak install $(
+RUN "sudo flatpak install $(
     for pkg in ${Flatpak[*]}; do
         printf '%s ' $pkg
     done
-)
+)"
 
 if [ $manager == "dnf" ]; then
     # abilita le user repository di Fedora (Copr)
     echo -e "$LI_INFO Abilitazione delle COPR preferite..."
     for repo in ${Copr[*]}; do
-        $DEBUG sudo $manager copr enable $repo
+        RUN "sudo $manager copr enable $repo"
     done
 
     # installa i pacchetti delle Copr
     echo -e "$LI_INFO Installazione pacchetti delle COPR..."
-    $DEBUG sudo $manager $install_cmd $(
+    RUN "sudo $manager $install_cmd $(
         for pkg in ${CoprPackages[*]}; do
             printf '%s ' $pkg
         done
-    )
+    )"
 fi
 
 # imposta la shell predefinita
 echo -e "$LI_INFO Cambio della shell predefinita a zsh..."
-$DEBUG sudo chsh -s "$(which zsh)" "$(whoami)"
+RUN "sudo chsh -s "$(which zsh)" "$(whoami)""
 
 # crea collegamenti simbolici
-for dest in ${!Symlinks[@]}; do
-    echo -e "$LI_INFO Creazione symlink '$(DIM_TEXT $dest)' --> '$(DIM_TEXT ${Symlinks[$dest]})'"
-    $DEBUG ln -s "$dest" "${Symlinks[$dest]}"
-done
+read -p "$(echo -e "$LI_INPUT Creare collegamenti simbolici da $(DIM_TEXT "$SCRIPT_DIR") a $(DIM_TEXT "~/.config")? (Y/N): ")" confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+    RUN "find $SCRIPT_DIR -mindepth 1 -maxdepth 1 -type d,f -not -path "*/.git*" -print0 | xargs -I{} -n1 -0 ln -s {} "~/.config""
+fi
 
 # avviso supporto hyprland su vm
 which 'Hyprland' > /dev/null
