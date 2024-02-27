8aebf7caff1a6b093d80b2b6c7a18b60ffc089ee
lu-papagni <89859659+lu-papagni@users.noreply.github.com>
2024-02-27 20:16:10 +0100

    Create definitions.sh
    File definizioni per kickstart.sh

---
+++ kickstart/utils/definitions.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Colori
+HI_DIM="\e[1;30m"    # Grigio
+HI_WARN="\e[1;33m"   # Giallo
+HI_OK="\e[1;32m"     # Verde
+HI_ERR="\e[1;31m"    # Rosso
+HI_RST="\e[0m"       # Reset
+HI_INV="\e[7m"       # Inverti
+
+# Decorazioni di linea
+LI_INFO="$HI_OK::$HI_RST"
+LI_INPUT="$HI_WARN::$HI_RST"
+LI_ERROR="$HI_ERR::$HI_RST"
