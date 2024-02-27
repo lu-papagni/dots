46844a6a84b582a7ffcc8c55a2799e4b9757eed0
lu-papagni <89859659+lu-papagni@users.noreply.github.com>
2024-02-27 20:20:55 +0100

    Update and rename kickstart.sh to kickstart/kickstart.sh

--- kickstart/kickstart.sh
+++ kickstart/kickstart.sh
@@ -1,83 +1,9 @@
 #!/usr/bin/env bash
 
-################################   DICHIARAZIONI   #####################################
+source "utils/definitions.sh"
+source "utils/functions.sh"
 
-# Colori
-HI_DIM="\e[1;30m"    # Grigio
-HI_WARN="\e[1;33m"   # Giallo
-HI_OK="\e[1;32m"     # Verde
-HI_ERR="\e[1;31m"    # Rosso
-HI_RST="\e[0m"       # Reset
-HI_INV="\e[7m"       # Inverti
-
-# Decorazioni di linea
-LI_INFO="$HI_OK::$HI_RST"
-LI_INPUT="$HI_WARN::$HI_RST"
-LI_ERROR="$HI_ERR::$HI_RST"
-
-# Variabili importanti
-SCRIPT_DIR="$(dirname "$0")"    # Cartella da cui si sta eseguendo lo script 
-
-# rende il testo grigio
-function DimText {
-    echo "$HI_DIM$1$HI_RST"
-}
-
-# crea una scritta di warning
-function WarnText {
-    echo -e "$HI_WARN""WARNING> $1$HI_RST"
-}
-
-# crea una scritta di errore
-function ErrText {
-    echo -e "\e[1;31m"
-}
-
-# crea una scritta di suggerimento
-function TipText {
-    echo -e "$HI_OK""TIP> $1$HI_RST"
-}
-
-function WithPrivilege {
-    case "$1" in
-        "dnf" | "pacman")
-            echo "sudo $1"
-            ;;
-        "yay")
-            echo "$1"
-            ;;
-        *)
-            exit 1
-            ;;
-    esac
-}
-
-# esegue o mostra in versione debug un comando
-function Run {
-    if [ "$DEBUG" != true ]; then
-        $1
-    else
-        if [[ "$DEBUG" = true || "$LOG_ENABLED" = true ]]; then
-            echo -e "$(DimText 'DEBUG>') $1"
-        fi
-    fi
-}
-
-function PrintUsage {
-    echo "
-    |---------------------------------------------| GUIDA RAPIDA |----------------------------------------------|
-    |  Comandi disponibili:                                                                                     |
-    |  -d    ->    Modalità debug: i comandi non vengono eseguiti ma mostrati a schermo                         |
-    |  -s    ->    Non controlla se sono installati tutti i programmi necessari al funzionamento dello script.  |
-    |              È utile da usare in combinazione con -d                                                      |
-    |  -h    ->    Aiuto: mostra questo messaggio.                                                              |
-    |  -l    ->    Abilita il logging. I comandi verranno prima mostrati a schermo come nel debug e poi         |
-    |              eseguiti.                                                                                    |
-    |                                                                                                           |
-    |  NB: questo messaggio viene mostrato anche se la sintassi dei parametri è errata.                         |
-    |-----------------------------------------------------------------------------------------------------------|
-    "
-}
+SCRIPT_DIR="$(dirname "$0")"    # Cartella da cui si sta eseguendo lo script
 
 # pacchetti da installare
 RequiredPackages=(
