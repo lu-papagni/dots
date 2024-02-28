153693effa47782f61d56b818c5aebc62967fd8e
lu-papagni <89859659+lu-papagni@users.noreply.github.com>
2024-02-28 17:25:53 +0100

    Update definitions.sh
    Codice di escape sottolineatura

--- kickstart/utils/definitions.sh
+++ kickstart/utils/definitions.sh
@@ -7,6 +7,7 @@ HI_OK="\e[1;32m"     # Verde
 HI_ERR="\e[1;31m"    # Rosso
 HI_RST="\e[0m"       # Reset
 HI_INV="\e[7m"       # Inverti
+HI_UND="\e[4m"       # Sottolinea
 
 # Decorazioni di linea
 LI_INFO="$HI_OK::$HI_RST"
