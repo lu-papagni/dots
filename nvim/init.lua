1b14604c8197e3f223ab177de48234ac06ecfe7d
lu-papagni <papagniluca4@gmail.com>
2024-03-15 21:56:07 +0100

    nvim: snipped preambolo labinfo & icone in netrw

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,3 @@
-require("user.options")
 require("user.keymaps")
 
 -- Impostazione del tema
@@ -7,5 +6,7 @@ Colorscheme.current = "gruvbox-material"
 Colorscheme.transparent = true
 
 require("user.lazy")
+require("user.options")
+require("user.snippets")
 
 Colorscheme.apply(nil, 'mix')
