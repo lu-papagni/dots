04f9fbe429ddc997e333397631bb5b4d08f1b7fb
lu-papagni <papagniluca4@gmail.com>
2025-12-20 21:22:31 +0100

    chore(wsl): added aliases for common windows utilities

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -7,6 +7,12 @@ alias vim='nvim'
 alias zsh-config="$EDITOR ${ZDOTDIR:-$HOME/.zshrc}"
 alias zsh-reload="source ${ZDOTDIR:-$HOME}/.zshrc"
 
+if [[ -v WSLENV ]]; then
+	alias powershell="/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
+	alias pwsh="/mnt/c/Program\ Files/PowerShell/7/pwsh.exe"
+	alias explorer="/mnt/c/Windows/explorer.exe"
+fi
+
 # Alias specifici per Debian
 if [[ -r /etc/issue ]] && [[ $(grep -ic 'debian' /etc/issue) -ge 1 ]]; then
   alias bat='batcat'
